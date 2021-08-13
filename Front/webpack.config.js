d8470f0f2a98c758257429d8560f050b941be627
Victor Grachev <v.grachev@cft.ru>
2021-08-13 20:16:34 +0700

    Промежуточный коммит

--- Front/webpack.config.js
+++ Front/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -20,5 +21,31 @@ module.exports = {
       template: path.join(__dirname, 'src', 'index.html'),
     }),
     new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, 'src', 'favicon.ico'),
+          to: path.resolve(__dirname, 'public'),
+        },
+        {
+          from: path.join(__dirname, 'src', 'inMessage.mp3'),
+          to: path.resolve(__dirname, 'public'),
+        },
+      ],
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+  },
+  devtool: 'source-map',
 };
